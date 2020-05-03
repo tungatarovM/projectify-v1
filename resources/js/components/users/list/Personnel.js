3ee2257cd6f2181c57d6cca125e6b86887dade10
tungatarovM <tungatarov.96@mail.ru>
2020-05-03 20:41:35 +0500

    add add user modal

--- resources/js/components/users/list/Personnel.js
+++ resources/js/components/users/list/Personnel.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import MaterialTable from 'material-table';
 import { columns } from '../../../constants/user';
 import { deleteUser, addUser } from '../../../store/actions/users';
+import AddUserModal from "./AddUserModal";
+import { roles } from '../../../constants/user';
 
 const Personnel = ({ users, projects, deleteUser, addUser }) => {
-  console.log('users from Personnel', users);
-  const handleAddUser = (newUser) => {
-    console.log('new user', newUser);
-    addUser(newUser);
+  const [open, toggleModal] = useState(false);
+
+  const handleAddUser = (event) => {
+    toggleModal(true);
   };
 
   const handleDelete = (user) => {
@@ -16,35 +18,47 @@ const Personnel = ({ users, projects, deleteUser, addUser }) => {
   };
 
   return (
-    <MaterialTable
-      title="Персонал"
-      columns={columns}
-      data={users}
-      actions={[
-        {
-          icon: 'add',
-          tooltip: 'Add User',
-          isFreeAction: true,
-          onClick: (event, newUser) => alert('Click'),
-        },
-      ]}
-      editable={{
-        onRowDelete: (user) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              handleDelete(user);
-            }, 600);
-          }),
-      }}
-    />
+    <Fragment>
+      <AddUserModal
+        addUser={addUser}
+        modal={open}
+        toggleModal={toggleModal}
+        roles={roles}
+        projects={projects}
+      />
+      <MaterialTable
+        title="Персонал"
+        columns={columns}
+        data={users}
+        actions={[
+          {
+            icon: 'add',
+            tooltip: 'Add User',
+            isFreeAction: true,
+            onClick: (event) => handleAddUser(event),
+          },
+        ]}
+        editable={{
+          onRowDelete: (user) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+                handleDelete(user);
+              }, 600);
+            }),
+        }}
+      />
+    </Fragment>
+
   );
 };
 
-const mapStateToProps = ({ users }) => {
-  console.log('users from ', users);
+const mapStateToProps = ({ users, projects }) => {
+  console.log('users from personnel', users);
+  console.log('projects from personnel', projects);
   return {
     users: users.data,
+    projects: projects.data,
   }
 };
 
