78a1b7021cc184bf31a3343244dd3a9568f75c06
tungatarovM <tungatarov.96@mail.ru>
2020-05-03 10:51:23 +0500

    put Personnel component inside ManageUsers component

--- resources/js/components/dashboard/Content.js
+++ resources/js/components/dashboard/Content.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import {Grid, Container, Box, ListItemIcon, ListItemText, ListItem} from '@material-ui/core';
-import Personnel from '../users/Personnel';
-import Colleagues from '../users/Colleagues';
-import Copyright from "./Copyright";
-import {People} from "@material-ui/icons";
+import ManageUsers from "../users/ManageUsers";
 
 export default ({ classes }) => {
   return (
     <Container maxWidth="lg" className={classes.container}>
       <Grid container spacing={3}>
-        <Route exact path='/manager' component={Personnel} />
+        <Route exact path='/manager' component={ManageUsers} />
         <Route path='/test/router' render={() => <h1>Test Route </h1>} />
       </Grid>
     </Container>
